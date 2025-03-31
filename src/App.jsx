f56b6d133e53e711856adaf121929dcabf7e82ac
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-03-31 18:23:28 -0300

    Agregue fondo al Hero + Agregue contenedor de Secciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Characteristics from "./components/Characteristics";
@@ -6,21 +6,30 @@ import Price from "./components/Price";
 import AskedQuestion from "./components/AskedQuestion";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import './App.css'
-import './index.css'
+import "./App.css";
+import "./index.css";
 
 function App() {
   return (
     <>
       <Header />
-      <Hero />
-      <Characteristics />
-      <Price />
-      <AskedQuestion />
-      <Contact />
+      <div className="cont-img-hero">
+        <img
+          src="./assets/img/imgHero.jpg"
+          alt="Imagen de un escritorio de oficina"
+          className="img-hero"
+        />
+      </div>
+      <div className="contSections">
+        <Hero />
+        <Characteristics />
+        <Price />
+        <AskedQuestion />
+        <Contact />
+      </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
