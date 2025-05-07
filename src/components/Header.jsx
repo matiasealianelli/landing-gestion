6569c6d0a46dbbc8197039ae3924189829473d44
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-05-07 16:35:30 -0300

    Elimine logica del SignUp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import "/src/styles/Header.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -6,16 +5,6 @@ import SignUp from "./SignUp/SignUp.jsx";
 import "../styles/styles SignUp/SignUp.css";
 
 export default function Header() {
-  const [isSignUpActive, setSignUpActive] = useState(false);
-
-  const handleSignUpClick = () => {
-    setSignUpActive(true);
-  };
-
-  const handleCloseSignUp = () => {
-    setSignUpActive(false);
-  };
-
   let top = 0;
   window.addEventListener("scroll", function () {
     let header = document.querySelector("header");
@@ -56,12 +45,7 @@ export default function Header() {
             <button
               title="Registrate ahora"
               className="btn-user btn-user-signup"
-              id="btn-user-signup"
-              onClick={() => {
-                let nav = document.querySelector(".list-header");
-                nav.classList.remove("active");
-                handleSignUpClick();
-              }}>
+              id="btn-user-signup">
               Registrarse
             </button>
             <button title="Inicia sesion ahora" className="btn-user">
@@ -108,28 +92,11 @@ export default function Header() {
               nav.classList.remove("active");
               btnMenu.style.display = "block";
               btnMenuExit.style.display = "none";
-              handleCloseSignUp();
             }
           }}>
           <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
         </svg>
       </header>
-      <div className={`conteiner-signup ${isSignUpActive ? "active" : ""}`}>
-        <div className="conteiner-card">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            onClick={handleCloseSignUp}
-            className="bi bi-x-circle close-signup"
-            viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-          </svg>
-          <SignUp />
-        </div>
-      </div>
     </>
   );
 }
