86ba486eb302915b3531b66b91a6db3e05dadb03
matiasealianelli <hola.matiasalianelli@gmail.com>
2025-05-15 16:53:36 -0300

    Agregue estructura jsx de Price

--- src/components/Price/CardsPrice.jsx
+++ src/components/Price/CardsPrice.jsx
@@ -1,10 +1,72 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Keyboard, Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import "../../styles/styles Price/CardsPrice.css";
 import InfoCardsPrice from "./InfoCardsPrice.json";
+import "../../index.css";
 
 export default function CardPrice() {
-    return (
-        <>
-            
-        </>
-    );
+  return (
+    <>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={20}
+        keyboard={{
+          enabled: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          360: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          500: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+        }}
+        modules={[Keyboard, Pagination, Navigation]}
+        className="mySwiper">
+        {InfoCardsPrice.map((info, index) => (
+          <SwiperSlide className="card-price" key={info.id}>
+            <div className="conteiner-title-icon">
+              <h4 className="title-card-price">
+                {InfoCardsPrice[index].titulo}
+              </h4>
+              <i className="bi bi-boxes icon-box icon-card-price"></i>
+            </div>
+            <p className="description-card-price">
+              {InfoCardsPrice[index].descripcion}
+            </p>
+            <div className="utilidades-price">
+              {InfoCardsPrice[index].utilidades.map((utilidad, i) => (
+                <p key={i} className="utilidades-cards-price">
+                  {utilidad}
+                </p>
+              ))}
+            </div>
+            <div className="conteiner-descuento">
+              <p className="priceOld-card-price">
+                <span className="line-cross-out-price"></span>
+                Antes ${InfoCardsPrice[index].precioAntiguo}
+              </p>
+              <p className="percent-card-price">
+                Ahorra un {InfoCardsPrice[index].descuento}%
+              </p>
+            </div>
+            <p className="newPrice-card">
+              ${InfoCardsPrice[index].precioNuevo}
+            </p>
+            <button className="btn btn-press-price">Elegir plan</button>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
 }
--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -4,7 +4,9 @@ import CardPrice from "./CardsPrice.jsx"
 export default function Price() {
     return (
       <>
-        <article className="Price" id="Price">
+        <article className="price" id="price">
+          <h3 className="title-sections">Precios</h3>
+          <p className="text-section-price">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus provident, error, odio cum, impedit pariatur rerum corrupti veritatis dolor perferendis delectus aperiam perspiciatis adipisci! Aut eligendi eaque voluptatem nulla facilis!</p>
           <CardPrice />
         </article>
       </>
